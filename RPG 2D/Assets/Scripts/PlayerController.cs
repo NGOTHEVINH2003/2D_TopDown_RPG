92c50b973bb80ef153702ca85b40a1c39331e876
NgoTheVinh <vinhnthe170433@fpt.edu.vn>
2024-09-15 23:16:58 +0700

    Add Player idle and running Sprites/animation, add change direction.

--- RPG 2D/Assets/Scripts/PlayerController.cs
+++ RPG 2D/Assets/Scripts/PlayerController.cs
@@ -10,10 +10,15 @@ public class PlayerController : MonoBehaviour
     private Vector2 movement;
     private Rigidbody2D rb;
 
+    private Animator playerAnim;
+    private SpriteRenderer pSpriteRenderer;
+
     private void Awake()
     {
         //get all the required components at the start.
         pControl = new PlayerControl();
+        playerAnim = GetComponent<Animator>();
+        pSpriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -29,17 +34,42 @@ public class PlayerController : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        AdjustPlayerFacingDirection();
         Move();
     }
     private void PlayerInput()
     {
         //read movement input from the input system.
         movement = pControl.Movement.Move.ReadValue<Vector2>();
+        //set the variable in the animator to trigger animation.
+        if(movement.x !=0 || movement.y != 0)
+        {
+            playerAnim.SetBool("Moving", true);
+        }
+        else
+        {
+            playerAnim.SetBool("Moving", false);
+        }
+
     }
     private void Move()
     {
         //move player.
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        if(mousePos.x < playerScreenPoint.x)
+        {
+            pSpriteRenderer.flipX = true;
+        }
+        else
+        {
+            pSpriteRenderer.flipX=false;
+        }
     }
 
 }
